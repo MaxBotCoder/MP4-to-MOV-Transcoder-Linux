363065abfc04186b55e1450a02c5791b7cefbaff
maxbot2009 <51864059+maxbot2009@users.noreply.github.com>
2023-09-13 14:01:30 -0600

    Delete encoder3.0.sh

--- encoder3.0.sh
+++
@@ -1,7 +0,0 @@
-mkdir transcoded
-for i in *.mp4; do
-  filename=$(basename -- "$i")  # Extract the base name of the input file
-  filename_noext="${filename%.*}"  # Remove the extension
-  ffmpeg -i "$i" -vcodec mjpeg -q:v 2 -acodec aac -q:a 0 -f mov "transcoded/${filename_noext}.mov"
-done
-
